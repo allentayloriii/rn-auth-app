a58f692c754f88a0309b4f4482b424771fcda375
SoundtrkSamurai <furtimat3@yahoo.com>
2025-07-22 13:27:44 -0700

    Add registration screen and update navigation layout
    - Implemented a new registration screen with form validation using react-hook-form and zod.
    - Updated RootLayout to include navigation for the registration screen.
    - Enhanced the index screen with a link to the registration screen.
    - Added necessary dependencies for form handling and validation.

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,35 @@
-import { Text, View } from "react-native";
+import { COLORS } from "@/utils/colors";
+import { Link } from "expo-router";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Link href="/register" asChild>
+        <TouchableOpacity style={styles.outlineButton}>
+          <Text style={styles.outlineButtonText}>Register</Text>
+        </TouchableOpacity>
+      </Link>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    justifyContent: "center",
+  },
+  outlineButton: {
+    alignItems: "center",
+    borderWidth: 1,
+    borderColor: COLORS.primary,
+    padding: 12,
+    borderRadius: 8,
+  },
+  outlineButtonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "600",
+  },
+});
---
+++ app/register.tsx
@@ -0,0 +1,179 @@
+import { COLORS } from "@/utils/colors";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "expo-router";
+import React, { useState } from "react";
+import { Controller, useForm } from "react-hook-form";
+import {
+  ActivityIndicator,
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import * as z from "zod";
+
+const schema = z.object({
+  name: z.string().optional(),
+  email: z.email("Invalid email address"),
+  password: z
+    .string()
+    .min(6, "Password must be at least 6 characters long")
+    .max(32, "Password must not exceed 32 characters"),
+});
+
+type FormData = z.infer<typeof schema>;
+
+const Register = () => {
+  const [loading, setLoading] = useState(true);
+  const {
+    control,
+    handleSubmit,
+    trigger,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+    mode: "onChange",
+  });
+  const router = useRouter();
+
+  const onSubmit = (data: FormData) => {
+    console.log("Form Data:", data);
+    // Handle form submission logic here
+  };
+
+  return (
+    <View style={styles.container}>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={{ flex: 1, justifyContent: "center" }}
+      >
+        <Controller
+          control={control}
+          name="name"
+          render={({ field: { onChange, onBlur, value } }) => (
+            <View style={styles.inputContainer}>
+              <TextInput
+                style={styles.input}
+                onChangeText={onChange}
+                onBlur={onBlur}
+                value={value}
+                placeholder="Name (optional)"
+                placeholderTextColor={COLORS.placeholder}
+              />
+            </View>
+          )}
+        />
+        <Controller
+          control={control}
+          name="email"
+          render={({ field: { onChange, onBlur, value } }) => (
+            <View style={styles.inputContainer}>
+              <TextInput
+                autoCapitalize="none"
+                style={styles.input}
+                onChangeText={onChange}
+                onBlur={onBlur}
+                value={value}
+                placeholder="Email"
+                placeholderTextColor={COLORS.placeholder}
+                keyboardType="email-address"
+              />
+              {errors.email && (
+                <Text style={styles.errorText}>{errors.email.message}</Text>
+              )}
+            </View>
+          )}
+        />
+        <Controller
+          control={control}
+          name="password"
+          render={({ field: { onChange, onBlur, value } }) => (
+            <View style={styles.inputContainer}>
+              <TextInput
+                style={styles.input}
+                onChangeText={onChange}
+                onBlur={onBlur}
+                value={value}
+                placeholderTextColor={COLORS.placeholder}
+                secureTextEntry
+              />
+              {errors.password && (
+                <Text style={styles.errorText}>{errors.password.message}</Text>
+              )}
+            </View>
+          )}
+        />
+        <TouchableOpacity
+          disabled={!!errors.email || !!errors.password || loading}
+          style={[
+            styles.button,
+            !errors.email && !errors.password ? {} : styles.buttonDisabled,
+          ]}
+          onPress={handleSubmit(onSubmit)}
+        >
+          <Text style={[styles.buttonText]}>Sign Up</Text>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
+      {loading && (
+        <View style={styles.loadingOverlay}>
+          <ActivityIndicator size="large" color="#fff" />
+        </View>
+      )}
+    </View>
+  );
+};
+
+export default Register;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    justifyContent: "center",
+  },
+  inputContainer: {
+    marginVertical: 8,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: COLORS.primary,
+    borderRadius: 8,
+    padding: 10,
+    backgroundColor: COLORS.input,
+    height: 50,
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "600",
+  },
+  button: {
+    marginTop: 20,
+    backgroundColor: COLORS.primary,
+    padding: 15,
+    borderRadius: 8,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "600",
+  },
+  errorText: {
+    color: "red",
+    fontSize: 12,
+    marginTop: 4,
+    marginLeft: 10,
+  },
+  buttonDisabled: {
+    opacity: 0.5,
+  },
+  loadingOverlay: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: "rgba(0, 0, 0, 0.6)",
+    justifyContent: "center",
+    alignItems: "center",
+    zIndex: 1,
+  },
+});
